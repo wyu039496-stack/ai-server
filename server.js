058d377c1c831047667c16a1d590390f5ca52516
wyu039496-stack <wyu039496@gmail.com>
2026-02-15 23:37:26 +0700

    remove dotenv for render

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-dotenv.config();
 
 const app = express();
 app.use(cors());
